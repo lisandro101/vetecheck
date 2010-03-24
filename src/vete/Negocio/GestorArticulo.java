1a7adf9ac798b6d49a0030dd78ff66d60b984023
lisandro101 <lisandro101@31d0d736-884c-0410-8c69-3318cac82c90>
2010-03-24 00:45:05 +0000

    Modificaciones en la fachada de persistencia y en algunos gestores

--- src/vete/Negocio/GestorArticulo.java
+++ src/vete/Negocio/GestorArticulo.java
@@ -1,17 +1,7 @@
-/*
- * GestorArticulo.java
- * 
- * Created on 19/06/2007, 00:57:21
- * 
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package vete.Negocio;
 
-import com.db4o.ext.Db4oException;
 import java.util.HashMap;
-import java.util.Vector;
+import java.util.List;
 import vete.Entidad.Articulo;
 import vete.Persistencia.FachadaPersistencia;
 
@@ -20,96 +10,95 @@ import vete.Persistencia.FachadaPersistencia;
  * @author Lisandro
  */
 public class GestorArticulo {
-    
+
     static GestorArticulo instancia = null;
 
     public GestorArticulo() {
     }
-    
+
     /**
      * Este método devuelve una instancia del GestorArticulo.
      * @return GestorArticulo
-     */  
+     */
     public static synchronized GestorArticulo getInstancia() { //Aplicación del patron singleton
-            
+
         //Si no existe una instancia del GestorArticulo la crea
-        if( instancia == null ) {
+        if (instancia == null) {
             instancia = new GestorArticulo();
         }
-		
+
         return instancia;
-    }	
-    
-    public void ingresar(HashMap<String, Object> datosArticulo) throws Db4oException {
-        
+    }
+
+    public void ingresar(HashMap<String, Object> datosArticulo) {
+
         //Crea un Articulo a partir de la Hash
         Articulo articulo = armarArticulo(datosArticulo);
-        
+
         //Le asigna un uid generado al azar al Articulo
         articulo.generarUid();
-        
+
         //Persiste el Articulo
-        FachadaPersistencia.getInstancia().grabar(articulo, true);
-              
-     }
-    
-    public Vector<Articulo> buscar(HashMap<String, Object> datosArticulo) throws Db4oException {
-        
-    
-     
+        FachadaPersistencia.getInstancia().grabar(articulo);
+
+    }
+
+    public List<Articulo> buscar(HashMap<String, Object> datosArticulo) {
+
+
+
         //Crea un Articulo a partir de la Hash
         Articulo articulo = armarArticulo(datosArticulo);
-        
+
         //Busca Articulo en la persistencia
-        Vector<Articulo> articulos = FachadaPersistencia.getInstancia().buscarArticulo( articulo );
-    
+        List<Articulo> articulos = FachadaPersistencia.getInstancia().buscarArticulo(articulo);
+
         return articulos;
-        
+
     }
-    
-    public void actualizar(HashMap<String, Object> datosArticulo) throws Db4oException {
-        
+
+    public void actualizar(HashMap<String, Object> datosArticulo) {
+
         //Crea un Articulo a partir de la Hash
         Articulo articulo = armarArticulo(datosArticulo);
-        
+
         //Persiste el Articulo
-        Fachada.getInstancia().actualizar( articulo );
-        
+        FachadaPersistencia.getInstancia().actualizar(articulo);
+
     }
-    
-    public void actualizar(Articulo articulo) throws Db4oException {
-        
+
+    public void actualizar(Articulo articulo) {
+
         //Persiste el Articulo
-        Fachada.getInstancia().actualizar( articulo );
-        
+        FachadaPersistencia.getInstancia().actualizar(articulo);
+
     }
-    
-    public void eliminar(HashMap<String, Object> datosArticulo) throws Db4oException {
-        
+
+    public void eliminar(HashMap<String, Object> datosArticulo) {
+
         //Crea un Articulo a partir de la Hash
         Articulo articulo = armarArticulo(datosArticulo);
-        
+
         //Persiste el Articulo
-        Fachada.getInstancia().borrar( articulo );
-        
+        FachadaPersistencia.getInstancia().borrar(articulo);
+
     }
-    
-     public void eliminar(Articulo articulo) throws Db4oException {
-        
+
+    public void eliminar(Articulo articulo) {
+
         //Persiste el Articulo
-        Fachada.getInstancia().borrar( articulo );
-        
+        FachadaPersistencia.getInstancia().borrar(articulo);
+
     }
-    
-    public Articulo armarArticulo(HashMap<String, Object> datosArticulo){
-        
+
+    public Articulo armarArticulo(HashMap<String, Object> datosArticulo) {
+
         Articulo articulo = new Articulo();
-        
+
         //Recupera los datos del articulo de una Hash y construye un Articulo
         articulo.setUid((String) datosArticulo.get("uid"));
         articulo.setCodigo((String) datosArticulo.get("codigo"));
         articulo.setNombre((String) datosArticulo.get("nombre"));
-        articulo.setRubro((String) datosArticulo.get("rubro"));
         articulo.setCosto((Double) datosArticulo.get("costo"));
         articulo.setPrecio((Double) datosArticulo.get("precio"));
         articulo.setMargen((Double) datosArticulo.get("margen"));
@@ -119,13 +108,12 @@ public class GestorArticulo {
         articulo.setMinimo((Double) datosArticulo.get("minimo"));
         articulo.setMaximo((Double) datosArticulo.get("maximo"));
         articulo.setDetalle((String) datosArticulo.get("detalle"));
-        
-        
-	
-        
+
+
+
+
         //faltan Telefonos y Facturas       
-        
+
         return articulo;
     }
-
 }
--- src/vete/Negocio/GestorSeguridad.java
+++ src/vete/Negocio/GestorSeguridad.java
@@ -6,188 +6,191 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package vete.Negocio;
 
-import com.db4o.ext.Db4oException;
 import java.util.HashMap;
-import java.util.Vector;
+import java.util.List;
 import javax.swing.JOptionPane;
 import vete.Entidad.Perfil;
 import vete.Entidad.Permiso;
 import vete.Entidad.Usuario;
-import vete.Persistencia.Fachada;
+import vete.Persistencia.FachadaPersistencia;
 
 /**
  *
  * @author Lisandro
  */
 public class GestorSeguridad {
-    
+
     private static GestorSeguridad instancia = null;
 
     public GestorSeguridad() {
     }
-    
+
     public static synchronized GestorSeguridad getInstancia() {
-    
-        if ( instancia == null ) {
+
+        if (instancia == null) {
             instancia = new GestorSeguridad();
         }
-        
+
         return instancia;
     }
-    
-    public void ingresarUsuario(HashMap<String, Object> datosUsuario)throws Db4oException {
-        
+
+    public void ingresarUsuario(HashMap<String, Object> datosUsuario) {
+
         //Crea un Usuario a partir de la Hash
         Usuario usuario = armarUsuario(datosUsuario);
-        
+
         //Le asigna un uid generado al azar al Usuario
         usuario.generarUid();
-        
+
         //Persiste el Usuario
-        Fachada.getInstancia().grabar( usuario );
+        FachadaPersistencia.getInstancia().grabar(usuario);
     }
-    
-    public void ingresarPerfil(HashMap<String, Object> datosPerfil)throws Db4oException {
-        
+
+    public void ingresarPerfil(HashMap<String, Object> datosPerfil) {
+
         //Crea un Perfil a partir de la Hash
         Perfil perfil = armarPerfil(datosPerfil);
-        
+
         //Le asigna un uid generado al azar al Perfil
         perfil.generarUid();
-        
+
         //Persiste el Usuario
-        Fachada.getInstancia().grabar( perfil );
+        FachadaPersistencia.getInstancia().grabar(perfil);
     }
-    
-    public void ingresarPermiso(HashMap<String, Object> datosPermiso)throws Db4oException {
-        
+
+    public void ingresarPermiso(HashMap<String, Object> datosPermiso) {
+
         //Crea un Permiso a partir de la Hash
         Permiso permiso = armarPermiso(datosPermiso);
-        
+
         //Le asigna un uid generado al azar al Permiso
         permiso.generarUid();
-        
+
         //Persiste el Usuario
-        Fachada.getInstancia().grabar( permiso );
+        FachadaPersistencia.getInstancia().grabar(permiso);
     }
-    
-    public Usuario armarUsuario(HashMap<String, Object> datosUsuario){
-        
+
+    public Usuario armarUsuario(HashMap<String, Object> datosUsuario) {
+
         Usuario usuario = new Usuario();
-        
+
         //Recupera los datos del usuario de una Hash y construye un Usuario
-        usuario.setNombre((String)datosUsuario.get("nombre"));
-        usuario.setContrasenia((String)datosUsuario.get("contrasenia"));
-	//usuario.setPerfil((Perfil)datosUsuario.get("perfil"));
-	
+        usuario.setNombre((String) datosUsuario.get("nombre"));
+        usuario.setContrasenia((String) datosUsuario.get("contrasenia"));
+        //usuario.setPerfil((Perfil)datosUsuario.get("perfil"));
+
         return usuario;
     }
-    
-    public Perfil armarPerfil(HashMap<String, Object> datosPerfil){
-        
+
+    public Perfil armarPerfil(HashMap<String, Object> datosPerfil) {
+
         Perfil perfil = new Perfil();
-        
+
         //Recupera los datos del perfil de una Hash y construye un Perfil
-        perfil.setNombre((String)datosPerfil.get("nombre"));
-        perfil.setDescripcion((String)datosPerfil.get("descripcion"));
-	perfil.setPermisos((Vector<Permiso>)datosPerfil.get("permisos"));
-        
-	
+        perfil.setNombre((String) datosPerfil.get("nombre"));
+        perfil.setDescripcion((String) datosPerfil.get("descripcion"));
+        perfil.setPermisos((List<Permiso>) datosPerfil.get("permisos"));
+
+
         return perfil;
     }
-    
-    public Permiso armarPermiso(HashMap<String, Object> datosPermiso){
-        
+
+    public Permiso armarPermiso(HashMap<String, Object> datosPermiso) {
+
         Permiso permiso = new Permiso();
-        
+
         //Recupera los datos del permiso de una Hash y construye un Permiso
-        permiso.setObjeto((String)datosPermiso.get("objeto"));
-        permiso.setLeer((Boolean)datosPermiso.get("leer"));
-	permiso.setInsertar((Boolean)datosPermiso.get("insertar"));
-        permiso.setActualizar((Boolean)datosPermiso.get("actualizar"));
-	permiso.setBorrar((Boolean)datosPermiso.get("borrar"));
-	
+        permiso.setObjeto((String) datosPermiso.get("objeto"));
+        permiso.setLeer((Boolean) datosPermiso.get("leer"));
+        permiso.setInsertar((Boolean) datosPermiso.get("insertar"));
+        permiso.setActualizar((Boolean) datosPermiso.get("actualizar"));
+        permiso.setBorrar((Boolean) datosPermiso.get("borrar"));
+
         return permiso;
     }
-    
-     public Vector<Usuario> buscarUsuario(HashMap<String, Object> datosUsuario) throws Db4oException {
-        
-    
-     
+
+    public List<Usuario> buscarUsuario(HashMap<String, Object> datosUsuario) {
+
+
+
         //Crea un Usuario a partir de la Hash
         Usuario usuario = armarUsuario(datosUsuario);
-        
+
         //Busca Cliente en la persistencia
-        Vector<Usuario> usuarios = Fachada.getInstancia().buscarUsuario( usuario );
-    
+        List<Usuario> usuarios = FachadaPersistencia.getInstancia().buscarUsuario(usuario);
+
         return usuarios;
-        
+
     }
-     
-      public Vector<Perfil> buscarPerfil(HashMap<String, Object> datosPerfil) throws Db4oException {
-        
-    
-     
+
+    public List<Perfil> buscarPerfil(HashMap<String, Object> datosPerfil) {
+
+
+
         //Crea un Perfil a partir de la Hash
         Perfil perfil = armarPerfil(datosPerfil);
-        
+
         //Busca Cliente en la persistencia
-        Vector<Perfil> perfiles = Fachada.getInstancia().buscarPerfil( perfil );
-    
+        List<Perfil> perfiles = FachadaPersistencia.getInstancia().buscarPerfil(perfil);
+
         return perfiles;
-        
+
     }
-    
-    public Vector<Perfil> buscarPerfil(Perfil perfil) throws Db4oException {
-        
+
+    public List<Perfil> buscarPerfil(Perfil perfil) {
+
         //Busca Perfil en la persistencia
-        Vector<Perfil> perfiles = Fachada.getInstancia().buscarPerfil( perfil );
-    
+        List<Perfil> perfiles = FachadaPersistencia.getInstancia().buscarPerfil(perfil);
+
         return perfiles;
-        
+
     }
-    
-    public Vector<Permiso> buscarPermiso(HashMap<String, Object> datosPermiso) throws Db4oException {
-        
-    
-     
+
+    public List<Permiso> buscarPermiso(HashMap<String, Object> datosPermiso) {
+
+
+
         //Crea un Permiso a partir de la Hash
         Permiso permiso = armarPermiso(datosPermiso);
-        
+
         //Busca Cliente en la persistencia
-        Vector<Permiso> permisos = Fachada.getInstancia().buscarPermiso( permiso );
-    
+        List<Permiso> permisos = FachadaPersistencia.getInstancia().buscarPermiso(permiso);
+
         return permisos;
-        
+
     }
-      
-    public boolean tienePermiso( Usuario usuario, String  objeto, String tipoPermiso){
-        
+
+    public boolean tienePermiso(Usuario usuario, String objeto, String tipoPermiso) {
+
         boolean puede = false;
-        
-        Vector<Permiso> permisos = usuario.getPerfil().getPermisos();
-        
+
+        List<Permiso> permisos = usuario.getPerfil().getPermisos();
+
         for (int i = 0; i <= permisos.size() - 1; i++) {
-            
-            Permiso permiso = permisos.elementAt(i);
-            
-            if ( permiso.getObjeto().equals( objeto ) ) {
-                
-                if ( tipoPermiso.equals( "INSERTAR" ) ) puede = permiso.getInsertar();
-                else if ( tipoPermiso.equals( "ACTUALIZAR" ) ) puede = permiso.getActualizar();
-                else if ( tipoPermiso.equals( "LEER" ) ) puede = permiso.getLeer();    
-                else if ( tipoPermiso.equals( "BORRAR" ) ) puede = permiso.getBorrar();
-                else JOptionPane.showMessageDialog(null, "ERROR. Permiso no existente.","Consulta Permiso", JOptionPane.ERROR_MESSAGE);
-                
+
+            Permiso permiso = permisos.get(i);
+
+            if (permiso.getObjeto().equals(objeto)) {
+
+                if (tipoPermiso.equals("INSERTAR")) {
+                    puede = permiso.getInsertar();
+                } else if (tipoPermiso.equals("ACTUALIZAR")) {
+                    puede = permiso.getActualizar();
+                } else if (tipoPermiso.equals("LEER")) {
+                    puede = permiso.getLeer();
+                } else if (tipoPermiso.equals("BORRAR")) {
+                    puede = permiso.getBorrar();
+                } else {
+                    JOptionPane.showMessageDialog(null, "ERROR. Permiso no existente.", "Consulta Permiso", JOptionPane.ERROR_MESSAGE);
                 }
-            
+
             }
-               
-        return puede;
-        
+
         }
 
+        return puede;
+
+    }
 }
